05d1774fcbf0e238502cddf25cb15708197c4625
S4001 <b8072600@newcastle.ac.uk>
2020-03-21 18:27:12 +0000

    Update connect 4.py

--- connect 4.py
+++ connect 4.py
@@ -18,6 +18,12 @@ def playerMove():
     c = int(input()) - 1
     board[findY(c)][c] = 2
 
+def testMove():
+    x = int(input()) - 1
+    y = int(input()) - 1
+    v = int(input())
+    board[y][x] = v
+
 def checkSame(c1, c2, c3, c4):
     if(c1 != 0 and c1 == c2 and c2 == c3 and c3 == c4):
         return c1
@@ -26,44 +32,54 @@ def checkSame(c1, c2, c3, c4):
 
 def checkWin():
     # horizontal check
-    for i in range(width - 4):
+    for i in range(width - 3):
         for j in range(height):
             c1 = board[j][i]
             c2 = board[j][i + 1]
             c3 = board[j][i + 2]
             c4 = board[j][i + 3]
-
-            return checkSame(c1, c2, c3, c4)
+            
+            v = checkSame(c1, c2, c3, c4)
+            if(v != 0):
+                return v
 
     # vertical check
     for i in range(width):
-        for j in range(height - 4):
+        for j in range(height - 3):
             c1 = board[j][i]
             c2 = board[j + 1][i]
             c3 = board[j + 2][i]
             c4 = board[j + 3][i]
 
-            return checkSame(c1, c2, c3, c4)
+            v = checkSame(c1, c2, c3, c4)
+            if(v != 0):
+                return v
 
     # diagonal check 1
-    for i in range(width - 4):
-        for j in range(height - 4):
+    for i in range(width - 3):
+        for j in range(height - 3):
             c1 = board[j][i]
             c2 = board[j + 1][i + 1]
             c3 = board[j + 2][i + 2]
             c4 = board[j + 3][i + 3]
 
-            return checkSame(c1, c2, c3, c4)
+            v = checkSame(c1, c2, c3, c4)
+            if(v != 0):
+                return v
 
     # diagonal check 2
-    for i in range(width - 4):
-        for j in range(height - 4):
+    for i in range(width - 3):
+        for j in range(height - 3):
             c1 = board[j][i]
             c2 = board[j + 1][i - 1]
             c3 = board[j + 2][i - 2]
             c4 = board[j + 3][i - 3]
 
-            return checkSame(c1, c2, c3, c4)
+            v = checkSame(c1, c2, c3, c4)
+            if(v != 0):
+                return v
+
+    return 0
 
 def show():
     print(board)
@@ -74,9 +90,13 @@ while not done:
     show()
     playerMove()
     
+    #testMove()
+    
     if(checkWin() == 1):
+        show()
         print("Computer Wins!")
         done = True
     elif(checkWin() == 2):
+        show()
         print("You Win!")
         done = True
